082a9fe686714c03ecb1a142009624ecd5ca684e
hazevich <t.d.chanturia@gmail.com>
2019-10-05 11:31:04 +0200

    deps and src

---
+++ src/LDGJ45/LDGJ45/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LDGJ45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
